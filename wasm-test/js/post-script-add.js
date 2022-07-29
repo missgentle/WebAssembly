1e43bcf87526b3b063aae9185fb100d837599e35
missgentle <33093616+missgentle@users.noreply.github.com>
2022-07-29 09:54:31 +0800

    Update post-script-add.js

--- wasm-test/js/post-script-add.js
+++ wasm-test/js/post-script-add.js
@@ -1,8 +1,8 @@
-__ATPOSTRUN__.push(function () {
-	// ͨ��Emscripten�ڲ���addFunction������������ע��һ�������������ظú����ĺ���ָ��
-	var newFuncPtr = Module.addFunction(function (num) {
+__ATPOSTRUN__.push(function() {
+	// 通过Emscripten内部的addFunction向函数索引表中注册一个函数，并返回该函数的函数指针
+	var newFuncPtr = Module.addFunction(function(num){
 		console.log(`Hello ${num} from JS!`);
 	}, "vi");
-	// ֱ�ӵ�����C/C++�����ж����wrapper����
-	Module.asm['wrapper'](newFuncPtr);
+	// 直接调用在C/C++代码中定义的wrapper函数
+	Module.asm['_wrapper'](newFuncPtr);
 });
